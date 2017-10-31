07f08f62d591d420609645f76297371776d3745c
Lucas Farias <lucas_fnf@hotmail.com>
2017-10-31 17:25:04 -0300

    Increase box of tiles

--- scripts/generate_regions_json.sh
+++ scripts/generate_regions_json.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-python generate_regions_json.py WRScornerPoints.csv regions.json 0 -75 -20 -32
+python generate_regions_json.py WRScornerPoints.csv regions.json 5 -75 -25 -32
 mv regions.json ../public/regions/regions.json
