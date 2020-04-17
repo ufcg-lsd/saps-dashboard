73415f509ad8ccc6b9579e9c629008a149e288a1
Wesley Monte <wesley.monte@ccc.ufcg.edu.br>
2020-04-17 12:40:26 -0300

    Added mount binds to dashboardApp.js on docker script

--- docker/helper.sh
+++ docker/helper.sh
@@ -16,7 +16,8 @@ run() {
   docker run -dit \
     --name "${CONTAINER_NAME}" \
     -p ${PORT}:80 \
-    -v "$(pwd)"/backend.config:/dashboard/backend.config:ro  \
+    -v "$(pwd)"/public/dashboardApp.js:/dashboard/public/dashboardApp.js \
+    -v "$(pwd)"/backend.config:/dashboard/backend.config  \
     "${DASHBOARD_REPO}":"${TAG}"
 }
 
